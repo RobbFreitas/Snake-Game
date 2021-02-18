6090fcba2351ffd492f4d4450f88f40244e0a626
Roberto Melo <robertocompromissos@gmail.com>
2021-02-18 10:09:23 -0300

    up

--- game/index.js
+++ game/index.js
@@ -1,4 +1,37 @@
+import { gameboard } from './Board/index.js';
+import { SNAKE_SPEED, draw as snakeDraw, update as snakeUpdate } from './snake/index.js'
+
 //const gameboard = document.getElementById('game-board'); 
 
 
-window.requestAnimationFrame()
+
+let LastTimeRender = 0;
+
+// current time -> miliseconds
+function main (currentTime){
+
+    window.requestAnimationFrame(main);
+
+    const secondsSinceLastRender = (currentTime - LastTimeRender) / 1000;
+
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+    LastTimeRender = currentTime;
+    console.log(LastTimeRender);
+
+    update(); {
+        snakeUpdate();
+    }
+
+    draw(); {
+        gameboard.innerHTML = '';
+        snakeDraw();
+    }
+
+}
+
+function update() {};
+
+function draw() {};
+
+window.requestAnimationFrame(main)
